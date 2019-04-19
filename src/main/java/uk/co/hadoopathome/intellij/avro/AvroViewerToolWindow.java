6d89bf35715bb9f867e7eff2b545153af9f1a916
Ben Watson <benwatson528@gmail.com>
2019-04-19 21:39:43 +0100

    Adding the schema and data tabs

--- src/main/java/uk/co/hadoopathome/intellij/avro/AvroViewerToolWindow.java
+++ src/main/java/uk/co/hadoopathome/intellij/avro/AvroViewerToolWindow.java
@@ -24,36 +24,59 @@ import java.util.List;
 public class AvroViewerToolWindow implements ToolWindowFactory {
     private static final Logger LOGGER = Logger.getInstance(AvroViewerToolWindow.class);
     private JPanel toolWindowContent;
-    private JTextPane textPane;
-
+    private JTabbedPane tabbedPane;
+    private JPanel schemaPanel;
+    private JTextPane schemaTextPane;
+    private JPanel dataPanel;
+    private JTextPane dataTextPane;
     private AvroFormatter avroFormatter = new AvroFormatter();
 
     public AvroViewerToolWindow() {
-        changePaneAlignment(this.textPane, StyleConstants.ALIGN_CENTER);
+        this.schemaTextPane.setEditable(false);
+        this.dataTextPane.setEditable(false);
+        changePaneAlignment(this.dataTextPane, StyleConstants.ALIGN_CENTER);
+        this.dataTextPane.setText("Drag and drop a .avro or .avsc file here");
+        this.tabbedPane.setEnabled(true);
+        this.dataTextPane.setDropTarget(createDropTarget());
+        this.schemaTextPane.setDropTarget(createDropTarget());
+    }
 
-        this.textPane.setText("Drag and drop a .avro or .avsc file here");
-        this.textPane.setDropTarget(new DropTarget() {
+    /**
+     * Creates the {@link DropTarget} instance to detect when files are dragged into a tab.
+     *
+     * @return the instantiated {@link DropTarget}
+     */
+    private DropTarget createDropTarget() {
+        return new DropTarget() {
             public void drop(DropTargetDropEvent evt) {
                 try {
                     evt.acceptDrop(DnDConstants.ACTION_COPY);
                     File file = ((List<File>) evt.getTransferable().getTransferData(DataFlavor.javaFileListFlavor)).get(0);
+                    LOGGER.info(String.format("Received file %s", file.getAbsolutePath()));
                     String formatted = avroFormatter.format(file);
-                    changePaneAlignment(textPane, StyleConstants.ALIGN_LEFT);
-                    textPane.setText(formatted);
+                    changePaneAlignment(dataTextPane, StyleConstants.ALIGN_LEFT);
+                    dataTextPane.setText(formatted);
+                    tabbedPane.setEnabled(true);
                 } catch (UnsupportedFlavorException | IOException e) {
-                    e.printStackTrace();
+                    JOptionPane.showMessageDialog(null, "Unable to read file");
                 }
             }
-        });
+        };
     }
 
     @Override
-    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+    public void createToolWindowContent(Project project, ToolWindow toolWindow) {
         ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
         Content content = contentFactory.createContent(this.toolWindowContent, "", false);
         toolWindow.getContentManager().addContent(content);
     }
 
+    /**
+     * Changes the alignment of text within a pane.
+     *
+     * @param textPane  the pane for which alignment should be changed
+     * @param alignment the desired alignment, as a {@link StyleConstants} value
+     */
     private void changePaneAlignment(JTextPane textPane, int alignment) {
         StyledDocument doc = textPane.getStyledDocument();
         SimpleAttributeSet position = new SimpleAttributeSet();
