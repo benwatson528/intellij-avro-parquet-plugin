51dd9f7a1f029a6178f8d779dbc0951848975018
Ben Watson <benwatson528@gmail.com>
2020-01-27 17:37:42 +0000

    Applying Google Formatter and analysing Checkstyle (#10)

--- src/main/java/uk/co/hadoopathome/intellij/avro/JTableHandler.java
+++ src/main/java/uk/co/hadoopathome/intellij/avro/JTableHandler.java
@@ -1,85 +1,86 @@
 package uk.co.hadoopathome.intellij.avro;
 
-import com.intellij.openapi.diagnostic.Logger;
-
-import javax.swing.*;
-import javax.swing.table.*;
-import java.awt.*;
+import java.awt.Component;
 import java.util.List;
+import javax.swing.DefaultCellEditor;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
+import javax.swing.table.TableModel;
 
-/**
- * Manages the JTable.
- */
+/** Manages the JTable. */
 class JTableHandler {
-    private static final Logger LOGGER = Logger.getInstance(JTableHandler.class);
-    private final JTable table;
+  private final JTable table;
 
-    JTableHandler(JTable table) {
-        this.table = table;
+  JTableHandler(JTable table) {
+    this.table = table;
+  }
+
+  /**
+   * Updates the contents of the JTable.
+   *
+   * @param records the raw JSON records to be added to the table
+   */
+  void updateTable(List<String> records) {
+    TableFormatter tableFormatter = new TableFormatter(records);
+    String[] columns = tableFormatter.getColumns();
+    String[][] rows = tableFormatter.getRows();
+    disableTableEditing();
+
+    TableModel tableModel = new DefaultTableModel(rows, columns);
+    this.table.setModel(tableModel);
+    resizeTableColumns();
+  }
+
+  /** Ensures that cells in the table can be copied but not modified. */
+  private void disableTableEditing() {
+    JTextField textField = new JTextField();
+    textField.setEditable(false);
+    DefaultCellEditor editor = new DefaultCellEditor(textField);
+    this.table.setDefaultEditor(Object.class, editor);
+  }
+
+  /**
+   * Automatically resizes the table columns to fit the content. Source:
+   * https://stackoverflow.com/a/17627497/729819.
+   */
+  private void resizeTableColumns() {
+    TableColumnModel columnModel = this.table.getColumnModel();
+    for (int columnCounter = 0; columnCounter < this.table.getColumnCount(); columnCounter++) {
+      TableColumn column = columnModel.getColumn(columnCounter);
+      int colWidth = getColHeaderWidth(column);
+
+      int maxRowWidth = 50;
+      for (int row = 0; row < this.table.getRowCount(); row++) {
+        TableCellRenderer cellRenderer = this.table.getCellRenderer(row, columnCounter);
+        Component rowComp = this.table.prepareRenderer(cellRenderer, row, columnCounter);
+        maxRowWidth = Math.max(rowComp.getPreferredSize().width, maxRowWidth);
+      }
+      if (maxRowWidth > colWidth && maxRowWidth > 300) {
+        column.setPreferredWidth(300);
+      } else {
+        column.setPreferredWidth(Math.max(colWidth, maxRowWidth) + 10);
+      }
     }
+  }
 
-    /**
-     * Updates the contents of the JTable.
-     *
-     * @param records the raw JSON records to be added to the table
-     */
-    void updateTable(List<String> records) {
-        TableFormatter tableFormatter = new TableFormatter(records);
-        String[] columns = tableFormatter.getColumns();
-        String[][] rows = tableFormatter.getRows();
-        disableTableEditing();
-
-        TableModel tableModel = new DefaultTableModel(rows, columns);
-        this.table.setModel(tableModel);
-        resizeTableColumns();
+  /**
+   * Finds the width of the text header of a given column.
+   *
+   * @param column the specific column whose width is to be defined
+   * @return the width of the column in pixels
+   */
+  private int getColHeaderWidth(TableColumn column) {
+    TableCellRenderer colRenderer = column.getCellRenderer();
+    if (colRenderer == null) {
+      colRenderer = this.table.getTableHeader().getDefaultRenderer();
     }
-
-    /**
-     * Ensures that cells in the table can be copied but not modified.
-     */
-    private void disableTableEditing() {
-        JTextField textField = new JTextField();
-        textField.setEditable(false);
-        DefaultCellEditor editor = new DefaultCellEditor(textField);
-        this.table.setDefaultEditor(Object.class, editor);
-    }
-
-    /**
-     * Automatically resizes the table columns to fit the content. Source: https://stackoverflow.com/a/17627497/729819.
-     */
-    private void resizeTableColumns() {
-        TableColumnModel columnModel = this.table.getColumnModel();
-        for (int columnCounter = 0; columnCounter < this.table.getColumnCount(); columnCounter++) {
-            TableColumn column = columnModel.getColumn(columnCounter);
-            int colWidth = getColHeaderWidth(column);
-
-            int maxRowWidth = 50;
-            for (int row = 0; row < this.table.getRowCount(); row++) {
-                TableCellRenderer cellRenderer = this.table.getCellRenderer(row, columnCounter);
-                Component rowComp = this.table.prepareRenderer(cellRenderer, row, columnCounter);
-                maxRowWidth = Math.max(rowComp.getPreferredSize().width, maxRowWidth);
-            }
-            if (maxRowWidth > colWidth && maxRowWidth > 300) {
-                column.setPreferredWidth(300);
-            } else {
-                column.setPreferredWidth(Math.max(colWidth, maxRowWidth) + 10);
-            }
-        }
-    }
-
-    /**
-     * Finds the width of the text header of a given column.
-     *
-     * @param column the specific column whose width is to be defined
-     * @return the width of the column in pixels
-     */
-    private int getColHeaderWidth(TableColumn column) {
-        TableCellRenderer colRenderer = column.getCellRenderer();
-        if (colRenderer == null) {
-            colRenderer = this.table.getTableHeader().getDefaultRenderer();
-        }
-        Component colComp = colRenderer.getTableCellRendererComponent(this.table, column.getHeaderValue(), false, false,
-                0, 0);
-        return colComp.getPreferredSize().width;
-    }
+    Component colComp =
+        colRenderer.getTableCellRendererComponent(
+            this.table, column.getHeaderValue(), false, false, 0, 0);
+    return colComp.getPreferredSize().width;
+  }
 }
--- src/test/java/uk/co/hadoopathome/intellij/avro/TableFormatterTest.java
+++ src/test/java/uk/co/hadoopathome/intellij/avro/TableFormatterTest.java
@@ -1,30 +1,29 @@
 package uk.co.hadoopathome.intellij.avro;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
-import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 public class TableFormatterTest {
-    @Test
-    public void testFormatTable() {
-        List<String> jsonRecords = Arrays.asList(
-                "{\"name\" : \"Joe Cole\", \"subject\" :  \"Science\"}",
-                "{\"name\" : \"Susan Jones\", \"subject\" :  \"Chemistry\"}"
-        );
+  @Test
+  public void testFormatTable() {
+    List<String> jsonRecords =
+        Arrays.asList(
+            "{\"name\" : \"Joe Cole\", \"subject\" :  \"Science\"}",
+            "{\"name\" : \"Susan Jones\", \"subject\" :  \"Chemistry\"}");
 
-        TableFormatter tableFormatter = new TableFormatter(jsonRecords);
-        Set<String> outputColumns = new HashSet<>(Arrays.asList(tableFormatter.getColumns()));
-        Set<String> expectedColumns = new HashSet<>(Arrays.asList("name", "subject"));
-        assertEquals(expectedColumns, outputColumns);
+    TableFormatter tableFormatter = new TableFormatter(jsonRecords);
+    Set<String> outputColumns = new HashSet<>(Arrays.asList(tableFormatter.getColumns()));
+    Set<String> expectedColumns = new HashSet<>(Arrays.asList("name", "subject"));
+    assertEquals(expectedColumns, outputColumns);
 
-        String[][] rows = tableFormatter.getRows();
-        Set<String> outputFirstRow = new HashSet<>(Arrays.asList(rows[0]));
-        Set<String> expectedFirstRow = new HashSet<>(Arrays.asList("Joe Cole", "Science"));
-        assertEquals(expectedFirstRow, outputFirstRow);
-    }
+    String[][] rows = tableFormatter.getRows();
+    Set<String> outputFirstRow = new HashSet<>(Arrays.asList(rows[0]));
+    Set<String> expectedFirstRow = new HashSet<>(Arrays.asList("Joe Cole", "Science"));
+    assertEquals(expectedFirstRow, outputFirstRow);
+  }
 }
