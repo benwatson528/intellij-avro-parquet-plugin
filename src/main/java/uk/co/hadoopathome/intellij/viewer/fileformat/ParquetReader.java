7d6a7689e126da3e076122cfba4968777412cd96
Ben Watson <benwatson528@gmail.com>
2020-02-02 17:36:05 +0000

    Failed attempts at getting basic Hadoop integration

--- src/main/java/uk/co/hadoopathome/intellij/viewer/fileformat/ParquetReader.java
+++ src/main/java/uk/co/hadoopathome/intellij/viewer/fileformat/ParquetReader.java
@@ -3,27 +3,27 @@ package uk.co.hadoopathome.intellij.viewer.fileformat;
 import com.intellij.openapi.diagnostic.Logger;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.parquet.column.page.PageReadStore;
-import org.apache.parquet.example.data.Group;
-import org.apache.parquet.example.data.simple.convert.GroupRecordConverter;
-import org.apache.parquet.hadoop.ParquetFileReader;
-import org.apache.parquet.hadoop.util.HadoopInputFile;
-import org.apache.parquet.io.ColumnIOFactory;
-import org.apache.parquet.io.MessageColumnIO;
-import org.apache.parquet.io.RecordReader;
-import org.apache.parquet.schema.MessageType;
-import org.apache.parquet.schema.Type;
-import org.codehaus.jettison.json.JSONException;
-import org.codehaus.jettison.json.JSONObject;
+// import org.apache.parquet.column.page.PageReadStore;
+// import org.apache.parquet.example.data.Group;
+// import org.apache.parquet.example.data.simple.convert.GroupRecordConverter;
+// import org.apache.parquet.hadoop.ParquetFileReader;
+// import org.apache.parquet.hadoop.util.HadoopInputFile;
+// import org.apache.parquet.io.ColumnIOFactory;
+// import org.apache.parquet.io.MessageColumnIO;
+// import org.apache.parquet.io.RecordReader;
+// import org.apache.parquet.schema.MessageType;
+// import org.apache.parquet.schema.Type;
+// import org.codehaus.jettison.json.JSONException;
+// import org.codehaus.jettison.json.JSONObject;
 
 public class ParquetReader implements Reader {
   private static final Logger LOGGER = Logger.getInstance(ParquetReader.class);
-  private final ParquetFileReader reader;
-  private final MessageType schema;
+  //  private final ParquetFileReader reader;
+  //  private final MessageType schema;
 
   /**
    * Source: https://stackoverflow.com/a/55476606/729819
@@ -32,16 +32,25 @@ public class ParquetReader implements Reader {
    */
   public ParquetReader(File file) throws IOException {
     System.out.println("parquet inside");
+    try {
+      Configuration conf = new Configuration();
 
-    Configuration conf = new Configuration();
-     //   conf.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
-        conf.set("fs.file.impl", org.apache.hadoop.fs.LocalFileSystem.class.getName());
-    conf.set("fs.file.impl", org.apache.hadoop.fs.LocalFileSystem.class.getName());
-    System.out.println("test");
+      conf.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
+      conf.set("fs.file.impl", org.apache.hadoop.fs.LocalFileSystem.class.getName());
 
-    Path path = new Path(file.getPath());
-    LOGGER.info("Path = " + path);
-    System.out.println("path = " + path);
+      Path path = new Path(file.getPath());
+
+      FileSystem fs = path.getFileSystem(conf);
+      System.out.println("FILE READ");
+      LOGGER.info("WE'VE READ THE FILE");
+    } catch (Exception e) {
+      System.out.println(e);
+      LOGGER.error("Uh oh", e);
+      throw new RuntimeException(e);
+    }
+
+    System.out.println("FILE READ");
+    LOGGER.info("WE'VE READ THE FILE");
 
     //
     //    ParquetMetadata readFooter = ParquetFileReader.readFooter(conf, path,
@@ -71,28 +80,33 @@ public class ParquetReader implements Reader {
 
     // add the decimal conversion to a generic data model
 
-    System.out.println("pathparquet = " + path);
-    HadoopInputFile inputFile = HadoopInputFile.fromPath(path, conf);
-    System.out.println("inputFile = " + inputFile);
-
-    this.reader = ParquetFileReader.open(inputFile);
-    System.out.println("reader = " + reader);
-
-    this.schema = reader.getFileMetaData().getSchema();
-
-    //    ParquetMetadata footer =
-    //        ParquetFileReader.readFooter(conf, path, ParquetMetadataConverter.NO_FILTER);
-    //    System.out.println("footer = "+footer);
-    // this.schema = footer.getFileMetaData().getSchema();
-    System.out.println("schema = " + schema);
-
-    // this.reader = new ParquetFileReader(conf, path, footer);
-    System.out.println("reader");
+    //    System.out.println("pathparquet = " + path);
+    //    HadoopInputFile inputFile = null;
+    //    try {
+    //    inputFile = HadoopInputFile.fromPath(path, conf);
+    //    } catch (Exception e) {
+    //      System.out.println(e);
+    //    }
+    //   // System.out.println("inputFile = " + inputFile);
+    //
+    //    this.reader = ParquetFileReader.open(inputFile);
+    //    System.out.println("reader = " + reader);
+    //
+    //    this.schema = reader.getFileMetaData().getSchema();
+    //
+    //    //    ParquetMetadata footer =
+    //    //        ParquetFileReader.readFooter(conf, path, ParquetMetadataConverter.NO_FILTER);
+    //    //    System.out.println("footer = "+footer);
+    //    // this.schema = footer.getFileMetaData().getSchema();
+    //    System.out.println("schema = " + schema);
+    //
+    //    // this.reader = new ParquetFileReader(conf, path, footer);
+    //    System.out.println("reader");
   }
 
   @Override
   public String getSchema() {
-    return this.schema.toString();
+    return ""; // this.schema.toString();
   }
 
   @Override
@@ -112,30 +126,31 @@ public class ParquetReader implements Reader {
     //
     //    return records;
 
-    System.out.println("getRecords");
-    int totalNumRecordsRead = 0;
-    PageReadStore pages;
-    List<String> records = new ArrayList<>();
-    while (null != (pages = this.reader.readNextRowGroup())) {
-      final long rows = pages.getRowCount();
-      final MessageColumnIO columnIO = new ColumnIOFactory().getColumnIO(this.schema);
-      final RecordReader<Group> recordReader =
-          columnIO.getRecordReader(pages, new GroupRecordConverter(this.schema));
-      for (int j = 0; j < rows; j++) {
-        System.out.println("in group");
-        final Group group = recordReader.read();
-        records.add(convertParquetGroupToJsonString(group));
-        totalNumRecordsRead++;
-        if (totalNumRecordsRead >= numRecords) {
-          LOGGER.info("Retrieved " + totalNumRecordsRead + " records");
-          System.out.println("Retrieved " + totalNumRecordsRead + " records");
-          return records;
-        }
-      }
-    }
-
-    LOGGER.info("Retrieved " + totalNumRecordsRead + " records");
-    return records;
+    //    System.out.println("getRecords");
+    //    int totalNumRecordsRead = 0;
+    //    PageReadStore pages;
+    //    List<String> records = new ArrayList<>();
+    //    while (null != (pages = this.reader.readNextRowGroup())) {
+    //      final long rows = pages.getRowCount();
+    //      final MessageColumnIO columnIO = new ColumnIOFactory().getColumnIO(this.schema);
+    //      final RecordReader<Group> recordReader =
+    //          columnIO.getRecordReader(pages, new GroupRecordConverter(this.schema));
+    //      for (int j = 0; j < rows; j++) {
+    //        System.out.println("in group");
+    //        final Group group = recordReader.read();
+    //        records.add(convertParquetGroupToJsonString(group));
+    //        totalNumRecordsRead++;
+    //        if (totalNumRecordsRead >= numRecords) {
+    //          LOGGER.info("Retrieved " + totalNumRecordsRead + " records");
+    //          System.out.println("Retrieved " + totalNumRecordsRead + " records");
+    //          return records;
+    //        }
+    //      }
+    //    }
+    //
+    //    LOGGER.info("Retrieved " + totalNumRecordsRead + " records");
+    //    return records;
+    return null;
   }
 
   /**
@@ -144,32 +159,32 @@ public class ParquetReader implements Reader {
    * @param group the Parquet Group (analogous to a row in any other file)
    * @return the JSONObject representing the record
    */
-  private String convertParquetGroupToJsonString(final Group group) {
-    JSONObject jsonObject = new JSONObject();
-
-    int fieldCount = group.getType().getFieldCount();
-    for (int field = 0; field < fieldCount; field++) {
-      int valueCount = group.getFieldRepetitionCount(field);
-      Type fieldType = group.getType().getType(field);
-      String fieldName = fieldType.getName();
-      for (int index = 0; index < valueCount; index++) {
-        if (fieldType.isPrimitive()) {
-          try {
-            jsonObject.put(fieldName, group.getValueToString(field, index));
-          } catch (JSONException e) {
-            LOGGER.error("Unable to convert object to Parquet");
-          }
-        } else {
-          try {
-            jsonObject.put(
-                fieldName, convertParquetGroupToJsonString(group.getGroup(field, index)));
-          } catch (JSONException e) {
-            e.printStackTrace();
-          }
-        }
-      }
-    }
-    System.out.println(jsonObject.toString());
-    return jsonObject.toString();
-  }
+  //  private String convertParquetGroupToJsonString(final Group group) {
+  //    JSONObject jsonObject = new JSONObject();
+  //
+  //    int fieldCount = group.getType().getFieldCount();
+  //    for (int field = 0; field < fieldCount; field++) {
+  //      int valueCount = group.getFieldRepetitionCount(field);
+  //      Type fieldType = group.getType().getType(field);
+  //      String fieldName = fieldType.getName();
+  //      for (int index = 0; index < valueCount; index++) {
+  //        if (fieldType.isPrimitive()) {
+  //          try {
+  //            jsonObject.put(fieldName, group.getValueToString(field, index));
+  //          } catch (JSONException e) {
+  //            LOGGER.error("Unable to convert object to Parquet");
+  //          }
+  //        } else {
+  //          try {
+  //            jsonObject.put(
+  //                fieldName, convertParquetGroupToJsonString(group.getGroup(field, index)));
+  //          } catch (JSONException e) {
+  //            e.printStackTrace();
+  //          }
+  //        }
+  //      }
+  //    }
+  //    System.out.println(jsonObject.toString());
+  //    return jsonObject.toString();
+  //  }
 }
