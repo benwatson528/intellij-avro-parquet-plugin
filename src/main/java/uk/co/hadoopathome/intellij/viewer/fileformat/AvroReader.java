cb2ae84a54287266dc8dc225ddb88426ab6117f4
Ben Watson <benwatson528@gmail.com>
2020-02-04 16:32:12 +0000

    Adding new line to end of file

--- src/main/java/uk/co/hadoopathome/intellij/viewer/fileformat/AvroReader.java
+++ src/main/java/uk/co/hadoopathome/intellij/viewer/fileformat/AvroReader.java
@@ -5,9 +5,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import org.apache.avro.Schema;
 import org.apache.avro.file.DataFileReader;
-import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericDatumReader;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.io.DatumReader;
@@ -26,27 +24,12 @@ public class AvroReader implements Reader {
     return this.dataFileReader.getSchema().toString(true);
   }
 
-  public Schema getSchema2() {
-    return this.dataFileReader.getSchema();
-  }
-
   @Override
   public List<String> getRecords(int numRecords) {
     int i = 0;
     List<String> records = new ArrayList<>();
     while (this.dataFileReader.hasNext() && i < numRecords) {
       records.add(this.dataFileReader.next().toString());
-      i++;
-    }
-    LOGGER.info(String.format("Retrieved %d records", i));
-    return records;
-  }
-
-  public List<GenericData.Record> getRecordsRecord(int numRecords) {
-    int i = 0;
-    List<GenericData.Record> records = new ArrayList<>();
-    while (this.dataFileReader.hasNext() && i < numRecords) {
-      records.add((GenericData.Record) this.dataFileReader.next());
       i++;
     }
     LOGGER.info(String.format("Retrieved %d records", i));
