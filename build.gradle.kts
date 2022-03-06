9fc082417f175246a12c5906de1edb150072488d
Ben Watson <benwatson528@gmail.com>
2022-03-06 18:47:14 +0000

    Adding tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.6.10"
     id("org.jetbrains.intellij") version "1.4.0"
-    id("maven-publish")
 }
 
 group = properties("pluginGroup")
@@ -66,78 +65,17 @@ tasks {
         gradleVersion = properties("gradleVersion")
     }
 
+    test {
+        useJUnitPlatform()
+        testLogging {
+            events("passed", "skipped", "failed")
+        }
+    }
+
     patchPluginXml {
         version.set(properties("pluginVersion"))
         sinceBuild.set(properties("pluginSinceBuild"))
-        untilBuild.set(properties("pluginUntilBuild"))
         pluginDescription.set(properties("pluginDescription"))
         changeNotes.set(properties("changeNotes"))
-//        // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
-//        pluginDescription.set(
-//                projectDir.resolve("README.md").readText().lines().run {
-//                    val start = "<!-- Plugin description -->"
-//                    val end = "<!-- Plugin description end -->"
-//
-//                    if (!containsAll(listOf(start, end))) {
-//                        throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
-//                    }
-//                    subList(indexOf(start) + 1, indexOf(end))
-//                }.joinToString("\n").run { markdownToHTML(this) }
-//        )
-//
-//        // Get the latest available change notes from the changelog file
-//        changeNotes.set(provider {
-//            changelog.run {
-//                getOrNull(properties("pluginVersion")) ?: getLatest()
-//            }.toHTML()
-//        })
     }
 }
-
-
-
-
-
-
-//
-//configurations.all {
-//    exclude module: 'log4j'
-//    exclude module: 'slf4j-log4j12'
-//}
-
-//test {
-//    useJUnitPlatform()
-//    testLogging {
-//        events "passed", "skipped", "failed"
-//    }
-//}
-//
-//intellij {
-//    updateSinceUntilBuild = false
-//    version '2017.1.6'
-//    patchPluginXml {
-//        sinceBuild("171")
-//        changeNotes """
-//      Now works on M1 Macs."""
-//    }
-//}
-//
-//buildSearchableOptions.enabled = false
-//
-//publishing {
-//    repositories {
-//        maven {
-//            name = "GitHubPackages"
-//            url = uri("https://maven.pkg.github.com/benwatson528/intellij-avro-parquet-plugin")
-//            credentials {
-//                username = System.getenv("USERNAME")
-//                password = System.getenv("TOKEN")
-//            }
-//        }
-//    }
-//    publications {
-//        maven(MavenPublication) {
-//            artifact file("build/distributions/intellij-avro-parquet-viewer-${project.version}.zip")
-//        }
-//    }
-//}
